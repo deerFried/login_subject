9ae82f9652695d3578ecd2853f3c2b8dac97dd61
deerFried <sejung.kwon@vingle.net>
2022-05-03 15:09:46 +0900

    - Add PhoneAuthentication model and route test

---
+++ src/api/routes/__test__/phone_authentication_spec.ts
@@ -0,0 +1,55 @@
+import { expect } from "chai";
+import * as Sinon from "sinon";
+
+import { routes } from "../";
+
+import { createResolver, sandbox } from "../../../__test__/helpers";
+
+import * as Presenters from "../../presenters";
+
+import { PhoneAuthenticationService } from "../../../services/phone_authentication";
+
+const resolve = createResolver(routes);
+
+describe("PhoneAuthentication Routes", () => {
+  let phoneAuthenticationStub: Sinon.SinonStub;
+
+  describe("#createPhoneAuthentication", () => {
+    beforeEach(() => {
+      phoneAuthenticationStub = sandbox.stub(PhoneAuthenticationService, "create")
+        .callsFake(async () => true);
+    });
+
+    it("should return success true", async () => {
+      const res = await resolve("POST", "/phone_authentication", {}, {}, JSON.stringify({
+        phone: "01011111111",
+      }));
+
+      expect(res.statusCode).to.be.eq(200);
+      expect(JSON.parse(res.body)).to.be.deep.eq(
+        await Presenters.SuccessShow.present(true),
+      );
+      expect(phoneAuthenticationStub.callCount).to.be.eq(1);
+    });
+  });
+
+  describe("#verifyPhoneAuthentication", () => {
+    beforeEach(() => {
+      phoneAuthenticationStub = sandbox.stub(PhoneAuthenticationService, "verify")
+        .callsFake(async () => true);
+    });
+
+    it("should return success true", async () => {
+      const res = await resolve("POST", "/phone_authentication/verify", {}, {}, JSON.stringify({
+        phone: "01011111111",
+        key: "test-key",
+      }));
+
+      expect(res.statusCode).to.be.eq(200);
+      expect(JSON.parse(res.body)).to.be.deep.eq(
+        await Presenters.SuccessShow.present(true),
+      );
+      expect(phoneAuthenticationStub.callCount).to.be.eq(1);
+    });
+  });
+});
